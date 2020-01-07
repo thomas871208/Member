6739802d7661f741a3a4ffcdf4ddd8df51bc84a3
miia <miia@com.tw>
2020-01-07 14:19:43 +0800

    1st

--- app/src/main/java/com/fju/member/MainActivity.java
+++ app/src/main/java/com/fju/member/MainActivity.java
@@ -2,14 +2,27 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    String nk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        Intent intent = getIntent();
+            nk = intent.getStringExtra("nickname");
+            if(nk == null){
+                setContentView(R.layout.activity_nickname);
+            }else {
+                setContentView(R.layout.activity_main);
+            }
+
+
+        }
+
+
+
 }
--- app/src/main/java/com/fju/member/NicknameActivity.java
+++ app/src/main/java/com/fju/member/NicknameActivity.java
@@ -2,13 +2,28 @@ package com.fju.member;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class NicknameActivity extends AppCompatActivity {
-
+    EditText nickname;
+    String nk;
+    SharedPreferences pres;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nickname);
+        nickname = findViewById(R.id.nickname);
+        nk = nickname.getText().toString();
+        pres = getSharedPreferences("example", MODE_PRIVATE);
+        pres.edit()
+                .putString("nickname", nk)
+                .commit();
+        Intent intent = new Intent();
+        intent.putExtra("nickname","0");
+        startActivity(intent);
+
     }
 }
